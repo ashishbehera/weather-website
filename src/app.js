4ef4ee1a99143ef54cd0b6d33ea14791f44bc0d3
Ashish Behera <ashbeher@publicisgroupe.net>
2019-07-20 13:41:12 +0100

    Setup App for Heroku

--- src/app.js
+++ src/app.js
@@ -1,42 +1,44 @@
 const path = require('path');
-const  express = require('express');
+const express = require('express');
 const hbs = require('hbs');
 const request = require('request');
 const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 
 //Define path for Express Config
-const publicDirectoryPath=path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname,'../templates/views');
-const partialsPath = path.join(__dirname,'../templates/partials');
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 //Setup handlebars engine and views location
-app.set('view engine','hbs');
-app.set('views',viewsPath);
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
 //Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
-app.get('',(req,res) => {
-    res.render('index',{
+app.get('', (req, res) => {
+    res.render('index', {
         title: 'Weather',
         name: 'Ashish Behera'
     });
 })
 
-app.get('/about',(req,res) => {
-    res.render('about',{
+app.get('/about', (req, res) => {
+    res.render('about', {
         title: 'About Me',
         name: 'Ashish Behera'
 
     });
 })
 
-app.get('/help',(req,res) => {
-    res.render('help',{
+app.get('/help', (req, res) => {
+    res.render('help', {
         helpText: 'This is some useful Text',
         title: 'Help',
         name: 'Ashish Behera'
@@ -51,21 +53,21 @@ const weather = {
     longitude: -67.89
 }
 
-app.get('/weather',(req,res) => {
+app.get('/weather', (req, res) => {
 
-    if(!req.query.address){
+    if (!req.query.address) {
         return res.send({
             error: 'Please provide an address'
         })
     } else {
-        geocode(req.query.address, (error, {latitude,longitude,location} = {}) => {
-            if(error) {
+        geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+            if (error) {
                 //return console.log(error);
-                return res.send({  error })
-            } 
-    
+                return res.send({ error })
+            }
+
             forecast(latitude, longitude, (error, forecastData) => {
-                if(error) {
+                if (error) {
                     //return console.log(error);
                     return res.send({ error })
                 }
@@ -76,19 +78,19 @@ app.get('/weather',(req,res) => {
                         address: req.query.address
                     });
             });
-    
+
         });
     }
-    
- 
+
+
 })
 
-app.get('/products', (req,res) => {
+app.get('/products', (req, res) => {
 
-    if(!req.query.search) {
+    if (!req.query.search) {
         return res.send({
             error: 'You must provide a search term'
-            
+
         })
     }
     console.log(req.query.search);
@@ -98,16 +100,16 @@ app.get('/products', (req,res) => {
 
 })
 
-app.get('/help/*',(req,res) => {
-    res.render('404',{
+app.get('/help/*', (req, res) => {
+    res.render('404', {
         errorMessage: 'Help article not found',
         title: 'Help Articles',
         name: 'Ashish Behera'
     })
 })
 
-app.get('*',(req,res) => {
-    res.render('404',{
+app.get('*', (req, res) => {
+    res.render('404', {
         errorMessage: 'page not found',
         title: '404',
         name: 'Ashish Behera'
@@ -115,6 +117,6 @@ app.get('*',(req,res) => {
 })
 
 
-app.listen(3000,() => {
-    console.log('Sever is up on port 3000.');
+app.listen(port, () => {
+    console.log('Sever is up on port ' + port);
 })
