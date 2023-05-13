10a66b94d9d6548858795df3e917a950ae79412d
ashishbehera <36751260+ashishbehera@users.noreply.github.com>
2023-05-13 21:13:19 +0100

    Update script.groovy

--- src/script.groovy
+++ src/script.groovy
@@ -1,5 +1,16 @@
 def buildApp () {
-  echo "Buiding App from External"
+    echo 'Buiding App from External'
+}
+
+def deployApp () {
+    echo 'Deploying App from External'
+    echo "Deployed code version is ${params.VERSION}"
+
+}
+
+def testApp () {
+    echo 'Testing App from External'
 }
 
 return this
+
